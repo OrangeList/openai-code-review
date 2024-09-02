2e8f604fe35af09798673cd57a9cb761e3eddfff
Tianshizhici <3189753307@qq.com>
2024-09-02 20:28:51 +0800

    feat:gpt code review

--- openai-code-review-sdk/src/test/java/cn/tianshizhici/middleware/sdk/ApiTest.java
+++ openai-code-review-sdk/src/test/java/cn/tianshizhici/middleware/sdk/ApiTest.java
@@ -26,7 +26,7 @@ public class ApiTest {
         String role = "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审，只给出结果不需要额外的声明。代码为:";
         String res = chatGPT.chat(
                 role +
-                        "int a;"
+                        "int a = 99;"
         );
         System.out.println(res);
     }
