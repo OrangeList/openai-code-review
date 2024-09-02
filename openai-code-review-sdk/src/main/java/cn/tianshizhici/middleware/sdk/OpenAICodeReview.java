e04fed1398897c9aec315ec992bd595d92911465
Tianshizhici <3189753307@qq.com>
2024-09-02 16:28:05 +0800

    feat:maven jar

--- openai-code-review-sdk/src/main/java/cn/tianshizhici/middleware/sdk/OpenAICodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/tianshizhici/middleware/sdk/OpenAICodeReview.java
@@ -1,7 +1,32 @@
 package cn.tianshizhici.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class OpenAICodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("测试执行");
+
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        // 读取git diff命令的输出，每读取一行就将其附加到StringBuilder中
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
 }
