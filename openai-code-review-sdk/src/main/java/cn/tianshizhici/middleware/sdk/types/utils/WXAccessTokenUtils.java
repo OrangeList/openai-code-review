6bd2371cab6bffe8fa580fc690d177d57f25e761
Tianshizhici <3189753307@qq.com>
2024-09-03 14:17:44 +0800

    feat:评审日志写入文件

---
+++ openai-code-review-sdk/src/main/java/cn/tianshizhici/middleware/sdk/types/utils/WXAccessTokenUtils.java
@@ -0,0 +1,75 @@
+package cn.tianshizhici.middleware.sdk.types.utils;
+
+import com.alibaba.fastjson2.JSON;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WXAccessTokenUtils {
+
+    private static final String APPID = "wx0f370e0873d37d06";
+    private static final String SECRET = "b9960e8c28ad066b0d6e6493782c1b7b";
+    private static final String GRANT_TYPE = "client_credential";
+    private static final String URL_TEMPLATE = "https://api.weixin.qq.com/cgi-bin/token?grant_type=%s&appid=%s&secret=%s";
+
+    public static String getAccessToken() {
+        try {
+            String urlString = String.format(URL_TEMPLATE, GRANT_TYPE, APPID, SECRET);
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // Print the response
+                System.out.println("Response: " + response.toString());
+
+                Token token = JSON.parseObject(response.toString(), Token.class);
+
+                return token.getAccess_token();
+            } else {
+                System.out.println("GET request failed");
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static class Token {
+        private String access_token;
+        private Integer expires_in;
+
+        public String getAccess_token() {
+            return access_token;
+        }
+
+        public void setAccess_token(String access_token) {
+            this.access_token = access_token;
+        }
+
+        public Integer getExpires_in() {
+            return expires_in;
+        }
+
+        public void setExpires_in(Integer expires_in) {
+            this.expires_in = expires_in;
+        }
+    }
+
+
+}
--- openai-code-review-sdk/src/test/java/cn/tianshizhici/middleware/sdk/ApiTest.java
+++ openai-code-review-sdk/src/test/java/cn/tianshizhici/middleware/sdk/ApiTest.java
@@ -1,11 +1,20 @@
 package cn.tianshizhici.middleware.sdk;
 
+import cn.tianshizhici.middleware.sdk.types.utils.WXAccessTokenUtils;
+import com.alibaba.fastjson2.JSON;
 import com.plexpt.chatgpt.ChatGPT;
 import com.plexpt.chatgpt.util.Proxys;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.Proxy;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class ApiTest {
 
@@ -28,6 +37,90 @@ public class ApiTest {
                         "int a = 99;"
         );
         System.out.println(res);
+    }
+
+    @Test
+    public void test_wx() {
+        String accessToken = WXAccessTokenUtils.getAccessToken();
+        System.out.println(accessToken);
+
+        Message message = new Message();
+        message.put("project", "big-market");
+        message.put("review", "feat: 新加功能");
+
+        String url = String.format("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s", accessToken);
+        sendPostRequest(url, JSON.toJSONString(message));
+    }
+
+
+    private static void sendPostRequest(String urlString, String jsonBody) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            try (Scanner scanner = new Scanner(conn.getInputStream(), StandardCharsets.UTF_8.name())) {
+                String response = scanner.useDelimiter("\\A").next();
+                System.out.println(response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Message {
+        private String touser = "o9KUB64bgNLE_Xy1hHFZscvkWUk4";
+        private String template_id = "cPLLnV1vAjih9C298FHAerlRs9Z_idcl9TOxwY3wI5Q";
+        private String url = "https://github.com/OrangeList/openai-code-review-log/blob/main/2024-09-03/6iHc3al4whyq.md";
+        private Map<String, Map<String, String>> data = new HashMap<>();
+
+        public void put(String key, String value) {
+            data.put(key, new HashMap<String, String>() {
+                {
+                    put("value", value);
+                }
+            });
+        }
+
+        public String getTouser() {
+            return touser;
+        }
+
+        public void setTouser(String touser) {
+            this.touser = touser;
+        }
+
+        public String getTemplate_id() {
+            return template_id;
+        }
+
+        public void setTemplate_id(String template_id) {
+            this.template_id = template_id;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public Map<String, Map<String, String>> getData() {
+            return data;
+        }
+
+        public void setData(Map<String, Map<String, String>> data) {
+            this.data = data;
+        }
     }
 
 
